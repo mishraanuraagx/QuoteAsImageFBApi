8644ad0e95e6fff14fd8be4dca621e12d09988ac
Anuraag Mishra <mishra.anuraag@yahoo.com>
2017-06-12 21:48:17 +0530

    Found a way to add text to image. Still far from req result, there's an API to get quotes in an Image but its paid. 1. Thinking about using JPanel to customise and produce dynamic images which means keeping font size, font style, height of string, no. of words length of string, text wrapping properly, Centered text ... few more factors to consider to produce readable quotes for post. 2. Else can can put constraints on quotes, and pre customise the quotes, to achieve desired results. 3.Another Option is write quotes manually on images and save them. very Lazy and time Consuming

--- src/main/java/QuoteAsImageFBApi/Application.java
+++ src/main/java/QuoteAsImageFBApi/Application.java
@@ -8,25 +8,48 @@ import com.restfb.Parameter;
 import com.restfb.types.FacebookType;
 import com.restfb.types.User;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
 
 public class Application {
   public static void main(String[] args) {
     String accessToken = "AddYourAccessTokenHere";
     FacebookClient fbClient = new DefaultFacebookClient(accessToken);
+//    try {
+//      FileInputStream fis = new FileInputStream(new File("assets/images/FocusWallpaper.png"));
+//      FacebookType response = fbClient.publish("me/photos", FacebookType.class,
+//          BinaryAttachment.with("FocusWallpaper.png",fis), Parameter.with("message","Focus2"));
+//      System.out.println("Done");
+//    } catch (FileNotFoundException e) {
+//      e.printStackTrace();
+//    }
+
+    final BufferedImage image;
     try {
-//      FileInputStream fis = new FileInputStream(new File("C:\\Users\\MAX\\Desktop\\QuotesAsImage\\FocusWallpaper.png"));
-      FileInputStream fis = new FileInputStream(new File("assets/images/FocusWallpaper.png"));
-      FacebookType response = fbClient.publish("me/photos", FacebookType.class,
-          BinaryAttachment.with("FocusWallpaper.png",fis), Parameter.with("message","Focus2"));
-      System.out.println("Done");
-    } catch (FileNotFoundException e) {
+      image = ImageIO.read(
+          new File("assets/images/FocusWallpaper.png"));
+      Graphics g = image.getGraphics();
+//      g.setFont(g.getFont().deriveFont(300f));
+      g.setFont(new Font("Comic Sans MS", Font.BOLD & Font.ITALIC, 100));
+      g.setColor(Color.DARK_GRAY);
+      g.drawString("Hello World! <p color=\"#00FF00\">NOTICE</p>", 100, 100);
+      g.dispose();
+
+      ImageIO.write(image, "png", new File("assets/images/test.png"));
+      System.out.println("Done2");
+    } catch (IOException e) {
       e.printStackTrace();
     }
-    User me = fbClient.fetchObject("me", User.class);
-    System.out.println(me.getName());
-    System.out.println(me.getBirthday());
+//    User me = fbClient.fetchObject("me", User.class);
+//    System.out.println(me.getName());
+//    System.out.println(me.getBirthday());
   }
 }
---
+++ src/main/java/QuoteAsImageFBApi/Loader/QuotesLoader.java
@@ -0,0 +1,13 @@
+package QuoteAsImageFBApi.Loader;
+
+
+public class QuotesLoader {
+  public static String[] quotes = {
+      "It is better to be feared than loved, if you cannot be both."
+  };
+
+  public static String randomisedQuote(){
+
+    return quotes[(int)(Math.random()*quotes.length)];
+  }
+}

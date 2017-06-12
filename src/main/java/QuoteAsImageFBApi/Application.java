9339f6ac6078a651a8cc1fb43c498574920f7a45
Anuraag Mishra <mishra.anuraag@yahoo.com>
2017-06-12 18:36:55 +0530

    can upload image from a relative path to Timeline read and prints user name and his DOB

--- src/main/java/QuoteAsImageFBApi/Application.java
+++ src/main/java/QuoteAsImageFBApi/Application.java
@@ -1,8 +1,32 @@
 package QuoteAsImageFBApi;
 
 
+import com.restfb.BinaryAttachment;
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Parameter;
+import com.restfb.types.FacebookType;
+import com.restfb.types.User;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Application {
   public static void main(String[] args) {
-    System.out.println("hello World");
+    String accessToken = "AddYourAccessTokenHere";
+    FacebookClient fbClient = new DefaultFacebookClient(accessToken);
+    try {
+//      FileInputStream fis = new FileInputStream(new File("C:\\Users\\MAX\\Desktop\\QuotesAsImage\\FocusWallpaper.png"));
+      FileInputStream fis = new FileInputStream(new File("assets/images/FocusWallpaper.png"));
+      FacebookType response = fbClient.publish("me/photos", FacebookType.class,
+          BinaryAttachment.with("FocusWallpaper.png",fis), Parameter.with("message","Focus2"));
+      System.out.println("Done");
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    User me = fbClient.fetchObject("me", User.class);
+    System.out.println(me.getName());
+    System.out.println(me.getBirthday());
   }
 }

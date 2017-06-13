cbcaaafb4725cc551b8b57d2b16861eece91df54
Anuraag Mishra <mishra.anuraag@yahoo.com>
2017-06-13 12:46:29 +0530

    JLabel, HTML can produce the required Result, Several Files has added for various techniques Needs some Formatting

--- src/main/java/QuoteAsImageFBApi/Application.java
+++ src/main/java/QuoteAsImageFBApi/Application.java
@@ -10,14 +10,18 @@ import com.restfb.types.User;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import javax.imageio.ImageIO;
+import QuoteAsImageFBApi.stringUtils.StringUtils;
 
 public class Application {
   public static void main(String[] args) {
@@ -34,15 +38,23 @@ public class Application {
 
     final BufferedImage image;
     try {
+      String text = "Hello World hhhhhhhhhhhhh!Hello World! <p color=\"#00FF00\">NOTICE</p>";
+      Font font = new Font("Comic Sans MS", Font.BOLD & Font.ITALIC, 100);
       image = ImageIO.read(
           new File("assets/images/FocusWallpaper.png"));
       Graphics g = image.getGraphics();
 //      g.setFont(g.getFont().deriveFont(300f));
       g.setFont(new Font("Comic Sans MS", Font.BOLD & Font.ITALIC, 100));
       g.setColor(Color.DARK_GRAY);
-      g.drawString("Hello World! <p color=\"#00FF00\">NOTICE</p>", 100, 100);
-      g.dispose();
+      g.drawString("Hello World! <p color=\"#00FF00\">NOTICE</p>", 0, 100);
 
+      FontMetrics fm= g.getFontMetrics(font);
+      Rectangle2D rect=fm.getStringBounds("Some Text",g);
+
+      List<String> textList= StringUtils.wrap(text, fm, (int) image.getWidth());
+      System.out.println(rect.getWidth() +" "+ rect.getHeight() +" "+ image.getWidth()+" "+image.getHeight());
+      System.out.println(textList);
+      g.dispose();
       ImageIO.write(image, "png", new File("assets/images/test.png"));
       System.out.println("Done2");
     } catch (IOException e) {
---
+++ src/main/java/QuoteAsImageFBApi/model/LabelRenderTest.java
@@ -0,0 +1,89 @@
+package QuoteAsImageFBApi.model;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class LabelRenderTest {
+
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater( new Runnable() {
+      public void run() {
+
+        String title = "<html><body style='width: 700px; padding: 5px;'>"
+            + "<h1 style=\"font-size:50px\">Do U C Me?</h1>"
+            + "<p style=\"font-size:40px\">Here is a long string that will wrap.  "
+            + "Here is a long string that will wrap.  "
+            + "Here is a long string that will wrap.  "
+            + "The effect we want is a multi-line label.</p>";
+
+        JFrame f = new JFrame("Label Render Test");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        BufferedImage image = new BufferedImage(
+            1200,
+            800,
+            BufferedImage.TYPE_INT_RGB);
+        Graphics2D imageGraphics = image.createGraphics();
+        GradientPaint gp = new GradientPaint(
+            20f,
+            20f,
+            Color.red,
+            1180f,
+            780f,
+            Color.orange);
+        imageGraphics.setPaint(gp);
+        imageGraphics.fillRect(0, 0, 1200, 800);
+
+        JLabel textLabel = new JLabel(title);
+        textLabel.setSize(textLabel.getPreferredSize());
+
+        Dimension d = textLabel.getPreferredSize();
+        BufferedImage bi = new BufferedImage(
+            d.width,
+            d.height,
+            BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = bi.createGraphics();
+        g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        g.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+        g.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+
+        g.setColor(new Color(255, 255, 255, 255));
+        g.fillRoundRect(
+            0,
+            0,
+            bi.getWidth(f),
+            bi.getHeight(f),
+            15,
+            10);
+        g.setColor(Color.black);
+        textLabel.paint(g);
+        try {
+          ImageIO.write(bi, "png", new File("assets/images/test.png"));
+          System.out.println("done bi");
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        Graphics g2 = image.getGraphics();
+        g2.drawImage(bi, 20, 20, f);
+
+        ImageIcon ii = new ImageIcon(image);
+        JLabel imageLabel = new JLabel(ii);
+
+        f.getContentPane().add(imageLabel);
+        f.pack();
+        f.setLocationByPlatform(true);
+
+        f.setVisible(true);
+      }
+    });
+  }
+}
